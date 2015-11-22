5f06023b76bdaab95c194cbae742235689ec34fd
kuwako <kuwako@leverages.jp>
2015-11-22 18:25:09 +0900

    スクレイピング動いてる

--- crawl.go
+++ crawl.go
@@ -19,6 +19,22 @@ var rxOk = regexp.MustCompile(`https://kirei-kaigi\.co\.jp(/.*)`)
 // urlに訪れた時の処理
 func (e *Ext) Visit(ctx *gocrawl.URLContext, res *http.Response, doc *goquery.Document) (interface{}, bool) {
 	// URL集める
+	fmt.Println(doc.title)
+	return nil, true
+}
 
-	return nul, true
+func (e *Ext) Filter(ctx *gocrawl.URLContext, isVisited bool) bool {
+	return !isVisited && rxOk.MatchString(ctx.NormalizedURL().String())
+}
+
+func main() {
+	ext := &Ext{&gocrawl.DefaultExtender{}}
+	// クローリングのオプション設定
+	opts := gocrawl.NewOptions(ext)
+	opts.MaxVisits = 10
+	opts.LogFlags = gocrawl.LogError
+	opts.CrawlDelay = 5 * time.Second
+
+	c := gocrawl.NewCrawlerWithOptions(opts)
+	c.Run("https://kirei-kaigi.jp")
 }
